8da6e8372b9a6b214dddeb411fb32f1a16bc9507
unknown <msbadreldin@gmail.com>
2020-03-28 23:38:44 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentDate', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log(error)
+        }
+    })
+    next()
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance')
+// })
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+})
+
+const port = process.env.port || 3000
+app.listen(port)
