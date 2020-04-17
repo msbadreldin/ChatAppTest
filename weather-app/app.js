bfa651dc3a24399d68dec9db62f49d0c08d73a3b
unknown <msbadreldin@gmail.com>
2020-04-17 21:33:46 +0300

    7th change

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,34 +1,34 @@
-const yargs = require('yargs')
-const location = require('./geocode/geocode')
-const getTemp = require('./weatherInfo/weather')
+// const yargs = require('yargs')
+// const location = require('./geocode/geocode')
+// const getTemp = require('./weatherInfo/weather')
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
 
-    .help()
-    .alias('help', 'h')
-    .argv;
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
+// var encodedAddress = encodeURIComponent(argv.address);
 
-location.geocode(encodedAddress, (errorMessage, result) => {
-    if (errorMessage) {
-        console.log(errorMessage)
-    } else {
-        getTemp.getTemp(result, (err, temprature) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(temprature);
-            }
-        })
-    }
+// location.geocode(encodedAddress, (errorMessage, result) => {
+//     if (errorMessage) {
+//         console.log(errorMessage)
+//     } else {
+//         getTemp.getTemp(result, (err, temprature) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log(temprature);
+//             }
+//         })
+//     }
 
-})
+// })
